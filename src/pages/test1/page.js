b5393cdcb950c5f399760db46bde1a0da8ac63f9
Hak Rathana <hakrathana1114@gmail.com>
2023-01-30 23:16:51 +0700

    set up project

---
+++ src/pages/test1/page.js
@@ -0,0 +1,9 @@
+import { Avatar, Button, Checkbox, FormControlLabel, Rating, TextField } from "@mui/material";
+import React from "react";
+
+const Test2=()=>{
+	return(
+<Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+	)
+}
+export default Test2
